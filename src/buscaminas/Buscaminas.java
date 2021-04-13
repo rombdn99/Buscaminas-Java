f958782e3bc8ee98907a38d6fe6003be859e7467
rombdn99 <55712933+rombdn99@users.noreply.github.com>
2021-04-13 11:28:56 +0200

    Add files via upload

---
+++ src/buscaminas/Buscaminas.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas;
+
+/**
+ *
+ * @author DAW2
+ */
+public class Buscaminas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana1 miventana= new Ventana1();
+        System.out.print("Empieza el juego, dificultad :");
+        /*Ventana2 ventana=new Ventana2();
+        ventana.creargraella(8,8,10);*/
+
+    }
+    
+}
